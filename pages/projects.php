70214dfbac094ba681f43f82fc30337fdf1b3fd1
xenialopez <xlopez1717@gmail.com>
2021-11-02 02:09:58 +0100

    projects, tot acabat

--- pages/projects.php
+++ pages/projects.php
@@ -1,99 +1,65 @@
 <?php include '../shared/header.php' ?>
 
 <div class="projects__container">
-    <h2>Proyectos</h2>
-    <div class="projects__wrapper">
-        <div class="project-casio">
-            <div class="projects-image">
-                <img src="../assets/images/gerard.jpeg" alt="">
+    <div class="projects__content">
+        <h2>Proyectos</h2>
+        <div class="projects__wrapper">
+
+            <div class="project-example">
+                    <img src="../assets/images/logofisiocrem.png" alt="">
+                    <h4>Fisiocrem - URIACH</h4>
+                    <a href="https://www.fisiocrem.es/" target="_blank" class="projects__button">Ver proyecto</a>
             </div>
 
-            <div class="projects-content">
-                <p>Lorem ipsum </p>
+            <div class="project-example">
+            <img src="../assets/images/logocasio.gif" alt="">
+                    <h4>Casio</h4>
+                    <a href="https://catalogocasio.es/" target="_blank" class="projects__button">Ver proyecto</a>
             </div>
+
+            <div class="project-example">
+            <img src="../assets/images/logocannabix.png" alt="">
+                    <h4>Cannabix - URIACH</h4>
+                    <a href="https://cannabix.es/" target="_blank" class="projects__button">Ver proyecto</a>
+            </div>
+
+            <div class="project-example">
+            <img src="../assets/images/logorosettaestudio.png" alt="">
+                    <h4>Rosetta Estudio</h4>
+                    <a href="https://rosettaestudio.com/" target="_blank" class="projects__button">Ver proyecto</a>
+            </div>
+
+            <div class="project-example">
+            <img src="../assets/images/logoaquadrink.png" alt="">
+                    <h4>AQUADRINK - BTW IBÉRICA</h4>
+                    <a href="https://www.aquadrink.es/es/" target="_blank" class="projects__button">Ver proyecto</a>
+            </div>
+
+            <div class="project-example">
+            <img src="../assets/images/logoklockner.png" alt="">
+                    <h4>Klockner</h4>
+                    <a href="https://klockner.rosettaestudio.net/" target="_blank" class="projects__button">Ver proyecto</a>
+            </div>
+
+            <div class="project-example">
+            <img src="../assets/images/logocistitus.png" alt="">
+                    <h4>Cistitus - URIACH</h4>
+                    <a href="https://www.cistitus.com/" target="_blank" class="projects__button">Ver proyecto</a>
+            </div>
+
+            <div class="project-example">
+            <img src="../assets/images/logohalibut.jpeg" alt="">
+                    <h4>Halibut - URIACH</h4>
+                    <a href="https://www.halibut.es/" target="_blank" class="projects__button">Ver proyecto</a>
+            </div>
+
+            <div class="project-example">
+            <img src="../assets/images/logoribesalat.png" alt="">
+                    <h4>RibéSalat</h4>
+                    <a href="https://ribesalat.com/" target="_blank" class="projects__button">Ver proyecto</a>
+            </div>   
         </div>
-
-        <div class="project-fisiocrem">
-            <div class="projects-image">
-                <img src="../assets/images/gerard.jpeg" alt="">
-            </div>
-
-            <div class="projects-content">
-                <p>Lorem ipsum </p>
-            </div>
-        </div>
-
-        <div class="project-cannabix">
-            <div class="projects-image">
-                <img src="../assets/images/gerard.jpeg" alt="">
-            </div>
-
-            <div class="projects-content">
-                <p>Lorem ipsum </p>
-            </div>
-        </div>
-
-        <div class="project-rosetta">
-            <div class="projects-image">
-                <img src="../assets/images/gerard.jpeg" alt="">
-            </div>
-
-            <div class="projects-content">
-                <p>Lorem ipsum </p>
-            </div>
-        </div>
-
-        <div class="project-aquadrink">
-            <div class="projects-image">
-                <img src="../assets/images/gerard.jpeg" alt="">
-            </div>
-
-            <div class="projects-content">
-                <p>Lorem ipsum </p>
-            </div>
-        </div>
-
-        <div class="project-klockner">
-            <div class="projects-image">
-                <img src="../assets/images/gerard.jpeg" alt="">
-            </div>
-
-            <div class="projects-content">
-                <p>Lorem ipsum </p>
-            </div>
-        </div>
-
-        <div class="project-cistitus">
-            <div class="projects-image">
-                <img src="../assets/images/gerard.jpeg" alt="">
-            </div>
-
-            <div class="projects-content">
-                <p>Lorem ipsum </p>
-            </div>
-        </div>
-
-        <div class="project-halibut">
-            <div class="projects-image">
-                <img src="../assets/images/gerard.jpeg" alt="">
-            </div>
-
-            <div class="projects-content">
-                <p>Lorem ipsum </p>
-            </div>
-        </div>
-
-        <div class="project-ribesalat">
-            <div class="projects-image">
-                <img src="../assets/images/gerard.jpeg" alt="">
-            </div>
-
-            <div class="projects-content">
-                <p>Lorem ipsum </p>
-            </div>
-        </div>
-
-    </div>
+    </div>    
 </div>
 
 <?php include '../shared/footer.php' ?>
